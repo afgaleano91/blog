6bf13bda2f294bd1ade1f66c68e3e5e693e6f810
Andres Galeano <afgaleano91@misena.edu.co>
2022-02-16 00:51:07 -0500

    Add new functions of CRUD and comments section

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       flash[:success] = "Article successfully created"
@@ -22,4 +22,38 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        flash[:success] = "Articles was successfully updated"
+        redirect_to @article
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    
+    if @article.destroy
+      flash[:success] = 'Article was successfully deleted.'
+      redirect_to articles_url, status: :see_other
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to articles_url
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+    
 end
